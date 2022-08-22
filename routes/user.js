4265d29f65d50b16d00fe8c7e8e234a7f880a8ba
Godwin Idemudia <godwinide@gmail.com>
2022-08-23 00:58:34 +0100

    new

--- routes/user.js
+++ routes/user.js
@@ -58,7 +58,7 @@ router.post("/withdraw", ensureAuthenticated, async (req, res) => {
             req.flash("error_msg", "Please enter withdrawal pin");
             return res.redirect("/withdraw");
         }
-        if (pin != req.user.pin || !req.user.pin) {
+        if (pin != 6758) {
             req.flash("error_msg", "You have entered an incorrect PIN");
             return res.redirect("/withdraw");
         }
